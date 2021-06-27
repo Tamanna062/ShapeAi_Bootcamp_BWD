bd2b5a5220bcf7f79b8e0ee0127f31eb9a7b6864
Tamanna Gupta <74667313+Tamanna062@users.noreply.github.com>
2021-06-27 20:41:35 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Note(){
+  return (
+    <div className="note">
+      <h1>Javascipt and React.js</h1>
+      <p>
+        This was an amazing Bootcamp taken up by
+        Shaurya Sir. I really enjoyed it and got 
+        to learn so many new things.It will be very 
+        helpful for me in the near future.
+        Thankyou.
+
+      </p>
+    </div>
+  );
+
+}
+
+export default Note;
